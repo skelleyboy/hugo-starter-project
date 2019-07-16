393483ed4d34681af685d50bc7322fb6100d5b20
Christopher Geary <dev@crgeary.com>
2019-07-16 21:06:56 +0100

    Added basic PurgeCss support

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     plugins: [
+        require('@fullhuman/postcss-purgecss')({
+            content: [`${__dirname}/layouts/**/*.html`],
+            whitelist: [],
+        }),
         require('autoprefixer')({
             browsers: ['ie >= 8', 'last 3 versions']
-        })
+        }),
     ]
 };
